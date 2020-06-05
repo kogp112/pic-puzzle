4b5f7194fdd9f508e8b513d993b8cb6bd63287f9
kogp112 <gapkana@gmail.com>
2020-06-05 22:57:48 +0900

    add input file button

--- src/components/UploadButton/index.tsx
+++ src/components/UploadButton/index.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
 
 export default class UploadButton extends React.Component {
+  constructor(props: Readonly<{}>) {
+    super(props);
+    this.state = {
+      selectedFile: null
+    };
+  }
+  onFileChange = (event: React.FormEvent<HTMLInputElement>) => { 
+    // Update the state 
+    console.log(event.target);
+    this.setState({ selectedFile: event.target });
+  };
+
   render() {
     return (
       <>
+        <input type="file" onChange={this.onFileChange} />
         <button>UPLOAD</button>
       </>
     );
